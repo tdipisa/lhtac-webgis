e4f302d673dada5c09f4d03a739991781670b41e
tobia <tobia.dipisa@geo-solutions.it>
2016-05-12 12:01:36 +0200

    Fixing tests cases, initial step

---
+++ js/components/__tests__/Statistics-test.jsx
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2015, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const expect = require('expect');
+const React = require('react/addons');
+const ReactDOM = require('react-dom');
+
+const Statistics = require('../Statistics');
+
+describe('Statistics', () => {
+    beforeEach((done) => {
+        document.body.innerHTML = '<div id="container"></div>';
+        setTimeout(done);
+    });
+
+    afterEach((done) => {
+        ReactDOM.unmountComponentAtNode(document.getElementById("container"));
+        document.body.innerHTML = '';
+        setTimeout(done);
+    });
+
+    it('create a Statistics component', () => {
+        const statistics = ReactDOM.render(<Statistics/>, document.getElementById("container"));
+        expect(statistics).toExist();
+    });
+});
